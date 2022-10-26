100f2ae3a2a539e1c7d40e46ba2fa4330e887fe8
9yuhyeon <kkh2742@gmail.com>
2022-10-26 10:02:48 +0900

    Add views.index(get,post) in articles app

--- articles/models.py
+++ articles/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Article(models.Model):
 	title = models.CharField(max_length=100)
 	content = models.TextField(null=True, blank=True)
+
+	def __str__(self):
+		return str(self.title)
---
+++ articles/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from articles import views
+urlpatterns = [
+    path('', views.index.as_view(), name='index'),
+]
